d0358cf606d7ce083f6102ad1502d5bb5ea53f70
patins <patins@users.noreply.github.com>
2014-06-06 08:07:05 -0400

    relay

--- remote_launch.py
+++ remote_launch.py
@@ -2,8 +2,10 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web.resource import Resource
 from twisted.internet import reactor, ssl
+from twisted.python import log
+import sys, os, json
 
-import os, json
+RELAY_PIN = 17
 
 SSL_ROOT = 'ssl'
 sslContext = ssl.DefaultOpenSSLContextFactory(
@@ -14,7 +16,8 @@ sslContext = ssl.DefaultOpenSSLContextFactory(
 class RelayActivator(Resource):
     def render_GET(self, request):
         request.setHeader("content-type", "application/json")
-        # TODO activate the relay
+        reactor.callLater(0, GPIO.output, RELAY_PIN, GPIO.HIGH)
+        reactor.callLater(1, GPIO.output, RELAY_PIN, GPIO.LOW)
         return json.dumps({ 'success': True })
 
 
@@ -22,6 +25,8 @@ def create_static_child(file):
     file.putChild('static', File('static'))
 
 if __name__ == '__main__':
+    log.startLogging(sys.stdout)
+
     facade = File('facade')
     create_static_child(facade)
     facade.putChild('certificate.crt', File(os.path.join(SSL_ROOT, 'certificate.crt')))
@@ -31,5 +36,12 @@ if __name__ == '__main__':
     create_static_child(control)
     control.putChild('fire', RelayActivator())
     reactor.listenSSL(443, Site(control), contextFactory=sslContext,)
+
+    try:
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(RELAY_PIN, GPIO.OUT)
+    except ImportError:
+        log.err()
 
     reactor.run()
